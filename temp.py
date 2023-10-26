2fd654945bbf17bc1ed953ee396798080eb633f2
Jason Cheng <dumplinglife7584@gmail.com>
2023-10-26 18:09:51 -0400

    Create temp.py

---
+++ temp.py
@@ -0,0 +1,18 @@
+import torch
+from video_llama.models.video_llama import VideoLLAMA
+
+model = VideoLLAMA()
+
+batch_size = 32
+num_channels = 64
+time_length = 20
+height = 224
+width = 224
+num_audio_features = 20
+device = "cuda:0"
+
+image = torch.randn(batch_size, num_channels, time_length, height, width).to(device)
+
+inputs_llama, atts_llama = model.encode_videoQformer_visual(image)
+
+print(inputs_llama)
