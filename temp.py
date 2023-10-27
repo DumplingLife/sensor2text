c55ce1a673d97a59c7807fc196786d9a170d91dd
Jason Cheng <dumplinglife7584@gmail.com>
2023-10-27 15:24:12 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -4,12 +4,13 @@ from video_stuff import VideoSide
 model = VideoSide()
 
 batch_size = 8
-time_length = 10
+num_channels = 3
+time_length = 32
 height = 224
 width = 224
 device = "cuda:0"
 
-image = torch.randn(batch_size, 3, time_length, height, width).to(device)
+image = torch.randn(batch_size, num_channels, time_length, height, width).to(device)
 
 inputs_llama, atts_llama = model.encode_videoQformer_visual(image)
 
