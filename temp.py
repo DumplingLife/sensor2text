e710a3978d4191e8cbdb4c6a9aac5a89acc2654d
Jason Cheng <dumplinglife7584@gmail.com>
2023-10-27 15:16:15 -0400

    Update temp.py

--- temp.py
+++ temp.py
@@ -3,12 +3,11 @@ from video_stuff import VideoSide
 
 model = VideoSide()
 
-batch_size = 32
-num_channels = 64
-time_length = 20
+batch_size = 8
+num_channels = 16
+time_length = 10
 height = 224
 width = 224
-num_audio_features = 20
 device = "cuda:0"
 
 image = torch.randn(batch_size, num_channels, time_length, height, width).to(device)
