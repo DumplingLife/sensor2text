44ef021dd34d8b7f02d25eecff2f4ee2102eccfc
Jason Cheng <dumplinglife7584@gmail.com>
2023-11-01 18:54:47 -0400

    Update my_global_settings.py

--- my_global_settings.py
+++ my_global_settings.py
@@ -1,6 +1,6 @@
 settings = {
     "get_features": False,
     "feature_name": "",
-    "use_emg_embedding": False,
-    # "use_emg_embedding": True,
+    # "use_emg_embedding": False,
+    "use_emg_embedding": True,
 }
