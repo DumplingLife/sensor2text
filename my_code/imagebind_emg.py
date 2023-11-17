72557d7fc7146b8c070ad82dc326b6a33ea69548
Jason Cheng <dumplinglife7584@gmail.com>
2023-11-16 21:36:13 -0500

    Update imagebind_emg.py

--- my_code/imagebind_emg.py
+++ my_code/imagebind_emg.py
@@ -10,7 +10,7 @@ model, emb_size = imagebind_model.imagebind_huge()
 model.load_state_dict(torch.load("../Video-LLaMA-2-7B-Finetuned/imagebind_huge.pth"))
 
 video_dir = os.path.abspath(os.path.join(os.getcwd(), "../actionsense_data/videos_processed/2022-06-07_18-11-37_S00_eye-tracking-video-worldGaze_frame"))
-video_paths = [f"{video_dir}/video_{x}.avi" for x in range(230)]
+video_paths = [f"{video_dir}/video_{x:03d}.avi" for x in range(230)]
 
 inputs = {
     ModalityType.VISION: data.load_and_transform_video_data(video_paths, device)
