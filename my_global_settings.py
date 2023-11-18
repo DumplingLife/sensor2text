63b7e0c4e9fa65dc0d20fd9cb9f14253f8d23014
Jason Cheng <dumplinglife7584@gmail.com>
2023-11-17 21:01:05 -0500

    Update my_global_settings.py

--- my_global_settings.py
+++ my_global_settings.py
@@ -1,6 +1,11 @@
 settings = {
     "get_features": False,
     "feature_name": "",
-    "use_emg_embedding": False,
     # "use_emg_embedding": True,
+    "use_emg_embedding": False,
+    # "use_imagebind_embedding": True
+    "use_imagebind_embedding": False
 }
+
+# turning both of these on is weird, probably bug
+assert not (settings["use_emg_embedding"] and settings["use_imagebind_embedding"])
