620008f03cf0d2ca6a9e3cd3804e36e3b610a2f1
Jason Cheng <dumplinglife7584@gmail.com>
2023-11-17 20:39:04 -0500

    Update train.py

--- my_code/train.py
+++ my_code/train.py
@@ -72,10 +72,14 @@ for i, (inputs, targets) in enumerate(DataLoader(dataset, batch_size=1, shuffle=
         targets_list[i] = targets
 
 mean_output = sum(outputs) / 230
+mean_targets = sum(targets_list) / 230
 squared_error_to_target = 0
 squared_error_to_mean = 0
+targets_squared_error_to_mean = 0
 for output, targets in zip(outputs, targets_list):
     squared_error_to_target += torch.mean((output - targets)**2)
     squared_error_to_mean += torch.mean((mean_output-output)**2)
+    targets_squared_error_to_mean += torch.mean((targets-mean_targets)**2)
 print(squared_error_to_target / 230)
 print(squared_error_to_mean / 230)
+print(targets_squared_error_to_mean / 230)
