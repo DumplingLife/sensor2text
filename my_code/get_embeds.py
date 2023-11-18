fbb7fec4715cc9c2bbd33ee2d80c0d3cc49557e4
Jason Cheng <dumplinglife7584@gmail.com>
2023-11-18 14:07:51 -0500

    Update get_embeds.py

--- my_code/get_embeds.py
+++ my_code/get_embeds.py
@@ -3,6 +3,7 @@ from video_llama.models.ImageBind.models.imagebind_model import ModalityType
 from video_llama.models.ImageBind import data
 import torch
 import numpy as np
+import os
 
 device = "cuda:0"
 
@@ -13,11 +14,15 @@ model.to(device)
 # Define batch size
 batch_size = 8
 
+video_count = 0
 video_dir = "actionsense_data/videos_processed/2022-06-07_18-11-37_S00_eye-tracking-video-worldGaze_frame"
+for file in os.listdir(video_dir):
+    if file.endswith(".mp4"):
+        video_count += 1
 
 # Process in batches
-for batch_start in range(0, 255, batch_size):
-    batch_end = min(batch_start + batch_size, 255)
+for batch_start in range(230, video_count, batch_size):
+    batch_end = min(batch_start + batch_size, video_count)
     print(f"Processing batch: {batch_start} to {batch_end}")
 
     video_paths = [f"{video_dir}/video_{x:03d}.mp4" for x in range(batch_start, batch_end)]
