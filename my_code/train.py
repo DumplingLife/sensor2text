d53ddce44b8dda2e91d784f698df8c0966ea485f
Jason Cheng <dumplinglife7584@gmail.com>
2023-11-19 12:32:21 -0500

    Update train.py

--- my_code/train.py
+++ my_code/train.py
@@ -14,6 +14,7 @@ class ActionSenseDataset(Dataset):
         target_dir = "actionsense_data/imagebind_targets_2s"
         self.data_files = []
         self.target_files = []
+        self.filepaths = []
 
         # data and targets might not match: directories might not match, and # files in each might not match
         common_subdirs = set(os.listdir(data_dir)) & set(os.listdir(target_dir))
@@ -23,6 +24,7 @@ class ActionSenseDataset(Dataset):
             for file in common_files:
                 self.data_files.append(f"{data_dir}/{subdir}/{file}")
                 self.target_files.append(f"{target_dir}/{subdir}/{file}")
+                self.filepaths.append(f"{subdir}/{file}")
 
     def __len__(self):
         return len(self.data_files)
@@ -30,7 +32,7 @@ class ActionSenseDataset(Dataset):
     def __getitem__(self, idx):
         data = np.load(self.data_files[idx])
         target = np.load(self.target_files[idx])
-        return torch.from_numpy(data).float(), torch.from_numpy(target).float()
+        return torch.from_numpy(data).float(), torch.from_numpy(target).float(), self.filepaths[idx]
 
 class ContrastiveLoss(nn.Module):
     def __init__(self, temperature=0.07):
@@ -62,7 +64,7 @@ loss_function = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
 for epoch in tqdm(range(epochs)):
-    for i, (inputs, targets) in enumerate(dataloader):
+    for i, (inputs, targets, _) in enumerate(dataloader):
         optimizer.zero_grad()
         outputs = model(inputs)
         loss = loss_function(outputs, targets)
@@ -78,10 +80,12 @@ print("Training Complete")
 # testing stuff
 outputs = [None] * len(dataset)
 targets_list = [None] * len(dataset)
-for i, (inputs, targets) in enumerate(DataLoader(dataset, batch_size=1, shuffle=True)):
+output_paths = [None] * len(dataset)
+for i, (inputs, targets, filepath) in enumerate(DataLoader(dataset, batch_size=1, shuffle=True)):
     with torch.no_grad():
         outputs[i] = model(inputs)
         targets_list[i] = targets
+        output_paths[i] = filepath
 
 print(outputs[50])
 print(outputs[100])
@@ -101,8 +105,6 @@ print(squared_error_to_target / len(dataset))
 print(squared_error_to_mean / len(dataset))
 print(targets_squared_error_to_mean / len(dataset))
 
-"""
-# save
+
 for i in range(len(dataset)):
-    np.save(f"actionsense_data/S00_imagebind_embeds_pred_2s/{i:03d}.npy", outputs[i])
-"""
+    np.save(f"actionsense_data/imagebind_pred_2s/{output_paths}.npy", outputs[i])
