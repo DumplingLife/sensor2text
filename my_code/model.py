d91971c6b3989bc906ff52f48f5e4994b8acb767
Jason Cheng <dumplinglife7584@gmail.com>
2023-12-01 22:57:00 -0500

    Update model.py

--- my_code/model.py
+++ my_code/model.py
@@ -18,35 +18,40 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
 
 class Model(nn.Module):
-    def __init__(self, input_size=16, d_model=256, nhead=8, num_layers=8, output_size=1024, dropout=0.1):
+    def __init__(self, input_size=16, d_model=256, nhead=8, num_layers=8, output_size=1024, dropout=0.1, use_cls_token=True, use_pos_encoder=True):
         super().__init__()
         self.input_size = input_size
-        self.cls_token = nn.Parameter(torch.randn(1, 1, input_size))
+        self.cls_token = nn.Parameter(torch.randn(1, 1, input_size)) if use_cls_token else None
         self.input_projection = nn.Linear(input_size, d_model)
-        self.pos_encoder = PositionalEncoding(d_model, dropout)
+        self.pos_encoder = PositionalEncoding(d_model, dropout) if use_pos_encoder else None
         self.encoder_layer = nn.TransformerEncoderLayer(d_model, nhead, dropout=dropout)
         self.encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=num_layers)
         self.output_projection = nn.Linear(d_model, output_size)
 
     def forward(self, x):
-        cls_tokens = self.cls_token.expand(x.size(0), -1, -1)
-        x = torch.cat((cls_tokens, x), dim=1)
+        if self.cls_token is not None:
+            cls_tokens = self.cls_token.expand(x.size(0), -1, -1)
+            x = torch.cat((cls_tokens, x), dim=1)
         x = self.input_projection(x)
-        x = self.pos_encoder(x)
+        if self.pos_encoder is not None:
+            x = self.pos_encoder(x)
         x = self.encoder(x)
-        cls_token_output = x[:, 0, :]
-        return self.output_projection(cls_token_output)
+        if self.cls_token is not None:
+            x = x[:, 0, :]
+        return self.output_projection(x)
 
 class AllSensorsModel(nn.Module):
     def __init__(self):
         super().__init__()
         self.input_sizes = {'eye': 2, 'emg': 16, 'tactile': 32, 'body': 66}
-        output_size = 128
+        modality_d_hidden = 128
         self.encoders = nn.ModuleDict({
-            modality: Model(input_size=input_size, d_model=128, nhead=4, num_layers=8, output_size=output_size, dropout=0.1)
+            modality: Model(input_size=input_size, d_model=modality_d_hidden, nhead=2, num_layers=2, output_size=modality_d_hidden, dropout=0.1,
+                use_cls_token=False, use_pos_encoder=True)
             for modality, input_size in self.input_sizes.items()
         })
-        self.output_projection = nn.Linear(output_size*len(self.input_sizes), 1024)
+        self.final_encoder = Model(input_size=len(self.input_sizes) * modality_d_hidden, d_model=128, nhead=2, num_layers=2, output_size=1024, dropout=0.1,
+            use_cls_token=True, use_pos_encoder=False)
 
     def forward(self, x):
         start = 0
@@ -58,4 +63,4 @@ class AllSensorsModel(nn.Module):
             encoded_modalities.append(encoded)
             start = end
         x = torch.cat(encoded_modalities, dim=1)
-        return self.output_projection(x)
+        return self.final_encoder(x)
