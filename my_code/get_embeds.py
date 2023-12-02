8d3bbf361c94bc4ee0a9f068971f23daafc658b6
Jason Cheng <dumplinglife7584@gmail.com>
2023-12-02 00:02:40 -0500

    add auto make dir

--- my_code/get_embeds.py
+++ my_code/get_embeds.py
@@ -6,6 +6,10 @@ import numpy as np
 import os
 import time
 
+input_dir = "actionsense_data/videos_processed_2s/2022-06-13_22-35-11_S02_eye-tracking-video-worldGaze_frame"
+output_dir = "actionsense_data/imagebind_targets_2s/S02_2"
+os.makedirs(output_dir)
+
 device = "cuda:0"
 
 model, emb_size = imagebind_model.imagebind_huge()
@@ -16,8 +20,7 @@ model.to(device)
 batch_size = 16
 
 video_count = 0
-video_dir = "actionsense_data/videos_processed_2s/2022-06-13_22-35-11_S02_eye-tracking-video-worldGaze_frame"
-for file in os.listdir(video_dir):
+for file in os.listdir(input_dir):
     if file.endswith(".mp4"):
         video_count += 1
 
@@ -26,7 +29,7 @@ for batch_start in range(0, video_count, batch_size):
     batch_end = min(batch_start + batch_size, video_count)
     print(f"Processing batch: {batch_start} to {batch_end}")
 
-    video_paths = [f"{video_dir}/video_{x:03d}.mp4" for x in range(batch_start, batch_end)]
+    video_paths = [f"{input_dir}/video_{x:03d}.mp4" for x in range(batch_start, batch_end)]
 
     inputs = {
         ModalityType.VISION: data.load_and_transform_video_data(video_paths, device)
@@ -39,5 +42,4 @@ for batch_start in range(0, video_count, batch_size):
 
     for i, video_idx in enumerate(range(batch_start, batch_end)):
         print(embeddings["vision"][i].shape)
-        output_dir = "actionsense_data/imagebind_targets_2s/S02_2"
         np.save(f"{output_dir}/{video_idx:03d}.npy", embeddings["vision"][i].cpu().numpy())
