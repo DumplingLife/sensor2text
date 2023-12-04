8254a79c4d49c4e63527d07c27cd75b91c9fefa1
Jason Cheng <dumplinglife7584@gmail.com>
2023-12-04 13:56:01 -0500

    Update train.py

--- my_code/train.py
+++ my_code/train.py
@@ -34,7 +34,7 @@ dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 # model = Model()
 model = AllSensorsModel()
-saved_state_dict = torch.load("best_model.pt")
+saved_state_dict = torch.load("my_code/best_model.pt")
 for modality, encoder in model.encoders.items():
     encoder_state_dict = encoder.state_dict()
     encoder_state_dict.update(saved_state_dict[f"encoders.{modality}.encoder"])
