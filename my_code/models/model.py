223e053c534012c6ec0fb6bf96dc48f4cbc06bc2
Jason Cheng <dumplinglife7584@gmail.com>
2023-12-04 14:10:42 -0500

    Update model.py

--- my_code/models/model.py
+++ my_code/models/model.py
@@ -29,7 +29,7 @@ class AllSensorsModel(nn.Module):
         super().__init__()
         self.input_sizes = {'eye': 2, 'emg': 16, 'tactile': 32, 'body': 66}
         self.encoders = nn.ModuleDict({
-            modality: Model(input_size=input_size, d_model=input_size, nhead=2, num_layers=4, output_size=input_size, dropout=0.1,
+            modality: Model(input_size=input_size, d_model=input_size, nhead=2, num_layers=2, output_size=input_size, dropout=0.1,
                 use_cls_token=True, use_pos_encoder=True)
             for modality, input_size in self.input_sizes.items()
         })
