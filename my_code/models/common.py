9b4ad50ec797a994e1ae84e583c19806b0e35216
Jason Cheng <dumplinglife7584@gmail.com>
2023-12-04 20:56:33 -0500

    Update common.py

--- my_code/models/common.py
+++ my_code/models/common.py
@@ -8,11 +8,12 @@ class PositionalEncoding(nn.Module):
 
         position = torch.arange(max_len).unsqueeze(1)
         div_term = torch.exp(torch.arange(0, d_model, 2) * (-torch.log(torch.tensor(10000.0)) / d_model))
+        self.weight = 0.1
         pe = torch.zeros(max_len, 1, d_model)
         pe[:, 0, 0::2] = torch.sin(position * div_term)
         pe[:, 0, 1::2] = torch.cos(position * div_term)
         self.register_buffer('pe', pe)
 
     def forward(self, x):
-        x = x + self.pe[:x.size(0)]
+        x = x + self.weight * self.pe[:x.size(0)]
         return self.dropout(x)
