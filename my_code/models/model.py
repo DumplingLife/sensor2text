b41bd73ec614be79dbea5665f616a18829cc7b0d
Jason Cheng <dumplinglife7584@gmail.com>
2023-12-04 20:15:47 -0500

    Update model.py

--- my_code/models/model.py
+++ my_code/models/model.py
@@ -42,7 +42,7 @@ class AllSensorsModel(nn.Module):
                 num_layers=2,
                 output_size=output_sizes[modality],
                 dropout=0.1,
-                use_input_projection=True,
+                use_input_projection=False,
                 use_cls_token=False,
                 use_pos_encoder=True
                 )
