013bc3b236d4cec34465a1e022c291a69669fbe4
Jason Cheng <dumplinglife7584@gmail.com>
2023-12-12 20:56:58 -0500

    Update train.py

--- my_code/train.py
+++ my_code/train.py
@@ -11,14 +11,19 @@ from my_code.losses import ContrastiveLoss
 
 learning_rate = 0.0003
 batch_size = 32
-epochs = 60
+epochs = 2
 
 dataset = ConcatDataset([
     ActionsenseDataset("actionsense_data/all_sensors_2s", "actionsense_data/imagebind_targets_2s", "video"),
     ActionsenseDataset("actionsense_data/all_sensors_2s", "actionsense_data/imagebind_targets_text_2s", "text"),
 ])
+test_dataset = ConcatDataset([
+    ActionsenseDataset("actionsense_data/all_sensors_2s_test", "actionsense_data/imagebind_targets_2s", "video"),
+    ActionsenseDataset("actionsense_data/all_sensors_2s_test", "actionsense_data/imagebind_targets_text_2s", "text"),
+])
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
-print("len(dataset):", len(dataset))
+print("# training data:", len(dataset))
+print("# testing data:", len(test_dataset))
 
 model = AllSensorsModel()
 
@@ -52,33 +57,37 @@ for epoch in tqdm(range(epochs)):
     print(f"Epoch [{epoch+1}/{epochs}], Loss: {epoch_loss / num_iters}, MSE Loss: {epoch_mse_loss / num_iters}")
 
 # testing stuff
-outputs = [None] * len(dataset)
-targets_list = [None] * len(dataset)
-output_paths = [None] * len(dataset)
-for i, (inputs, targets, filepath, _) in enumerate(DataLoader(dataset, batch_size=1, shuffle=True)):
-    with torch.no_grad():
-        outputs[i] = model(inputs)[0]
-        targets_list[i] = targets[0]
-        output_paths[i] = filepath[0]
+def evaluate(dataset, output_dir):
+    outputs = [None] * len(dataset)
+    targets_list = [None] * len(dataset)
+    output_paths = [None] * len(dataset)
+    for i, (inputs, targets, filepath, _) in enumerate(DataLoader(dataset, batch_size=1, shuffle=True)):
+        with torch.no_grad():
+            outputs[i] = model(inputs)[0]
+            targets_list[i] = targets[0]
+            output_paths[i] = filepath[0]
 
-print(outputs[50])
-print(outputs[100])
-print(targets_list[50])
-print(targets_list[100])
+    print(outputs[50])
+    print(outputs[100])
+    print(targets_list[50])
+    print(targets_list[100])
 
-mean_output = sum(outputs) / len(dataset)
-mean_targets = sum(targets_list) / len(dataset)
-squared_error_to_target = 0
-squared_error_to_mean = 0
-targets_squared_error_to_mean = 0
-for output, targets in zip(outputs, targets_list):
-    squared_error_to_target += torch.mean((output - targets)**2)
-    squared_error_to_mean += torch.mean((mean_output-output)**2)
-    targets_squared_error_to_mean += torch.mean((targets-mean_targets)**2)
-print("MSE:", squared_error_to_target.item() / len(dataset))
-print("average pred squared error to pred mean:", squared_error_to_mean.item() / len(dataset))
-print("average target squared error to target mean:", targets_squared_error_to_mean.item() / len(dataset))
+    mean_output = sum(outputs) / len(dataset)
+    mean_targets = sum(targets_list) / len(dataset)
+    squared_error_to_target = 0
+    squared_error_to_mean = 0
+    targets_squared_error_to_mean = 0
+    for output, targets in zip(outputs, targets_list):
+        squared_error_to_target += torch.mean((output - targets)**2)
+        squared_error_to_mean += torch.mean((mean_output-output)**2)
+        targets_squared_error_to_mean += torch.mean((targets-mean_targets)**2)
+    print("MSE:", squared_error_to_target.item() / len(dataset))
+    print("average pred squared error to pred mean:", squared_error_to_mean.item() / len(dataset))
+    print("average target squared error to target mean:", targets_squared_error_to_mean.item() / len(dataset))
 
+    for i in range(len(dataset)):
+        np.save(f"{output_dir}/{output_paths[i]}", outputs[i])
 
-for i in range(len(dataset)):
-    np.save(f"actionsense_data/imagebind_preds_2s/{output_paths[i]}", outputs[i])
+evaluate(dataset, "actionsense_data/imagebind_preds_2s")
+print("=" * 50)
+evaluate(test_dataset, "actionsense_data/imagebind_preds_2s")
